98f237ce77913a3a43b6f8fa9b86ae86e478de45
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-09 21:27:23 -0500

    Header component added

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+function Card (){
+    return(
+        <div className="card">
+        <button>count is 0</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR.
+
+          I could really use a taco right now.
+        </p>
+      </div>
+    );
+};
+
+export default Card;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+        <p className="read-the-docs">
+        Something about this smells like old socks...
+      </p>
+    );
+};
+
+export default Footer;
