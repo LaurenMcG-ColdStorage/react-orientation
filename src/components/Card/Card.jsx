8b60857bb064bb91611ac870bf35cfd1e6fe0c5b
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-09 21:47:46 -0500

    Playing with states

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,19 @@
+//To use state, use hooks
+//Object deconstruction
+import { useState } from 'react';
+
 function Card (){
+    const [myName, setMyName] = useState();
+
+    function clickChangeName(){
+        console.log('Test');
+        setMyName('Reili');
+    }
     return(
         <div className="card">
         <button>count is 0</button>
+        <p>{myNamed}</p>
+        <button onclick={clickChangeName}>Change Name</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR.
 
